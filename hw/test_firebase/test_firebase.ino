eb8520d74f896587957673a6c112e50bacbcb6e8
LayanJb <layanjabareen@gmail.com>
2022-12-06 11:02:27 +0200

    updated firebase_test

--- hw/test_firebase/test_firebase.ino
+++ hw/test_firebase/test_firebase.ino
@@ -23,8 +23,8 @@
 #include "addons/RTDBHelper.h"
 
 // Insert your network credentials
-#define WIFI_SSID "REPLACE_WITH_YOUR_SSID"
-#define WIFI_PASSWORD "REPLACE_WITH_YOUR_PASSWORD"
+#define WIFI_SSID "ICST"
+#define WIFI_PASSWORD "arduino123"
 
 // Insert Firebase project API Key
 #define API_KEY "AIzaSyBZVl5UmOnKJhjq-sCDZ9Vv70ZvF6dZ39c"
@@ -32,6 +32,10 @@
 // Insert RTDB URLefine the RTDB URL */
 #define DATABASE_URL "/" 
 
+//insert project email and id
+#define USER_EMAIL "smartlend.236333@gmail.com"
+#define USER_PASSWORD "sadProject!"
+#define FIREBASE_PROJECT_ID "smartlend-drawers"
 //Define Firebase Data object
 FirebaseData fbdo;
 
@@ -43,10 +47,11 @@ int count = 0;
 bool signupOK = false;
 
 void setup(){
+  Serial.println("Got to this stage");
   Serial.begin(115200);
- // WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-    delay(250);
-    WiFi.mode(WIFI_STA);
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  delay(250);
+    //WiFi.mode(WIFI_STA);
   Serial.print("Connecting to Wi-Fi");
   while (WiFi.status() != WL_CONNECTED){
     Serial.print(".");
@@ -64,23 +69,26 @@ void setup(){
   config.database_url = DATABASE_URL;
 
   /* Sign up */
-  if (Firebase.signUp(&config, &auth, "", "")){
-    Serial.println("ok");
-    signupOK = true;
-  }
-  else{
-    Serial.printf("%s\n", config.signer.signupError.message.c_str());
-  }
+  auth.user.email = USER_EMAIL;
+  auth.user.password = USER_PASSWORD;
+//  if (Firebase.signUp(&config, &auth, "", "")){
+//    Serial.println("ok");
+//    signupOK = true;
+//  }
+//  else{
+//    Serial.printf("%s\n", config.signer.signupError.message.c_str());
+//  }
 
   /* Assign the callback function for the long running token generation task */
   config.token_status_callback = tokenStatusCallback; //see addons/TokenHelper.h
   
   Firebase.begin(&config, &auth);
   Firebase.reconnectWiFi(true);
+  Serial.print("DONE SETUP!");
 }
 
 void loop(){
-  if (Firebase.ready() && signupOK && (millis() - sendDataPrevMillis > 15000 || sendDataPrevMillis == 0)){
+  if (Firebase.ready()&& (millis() - sendDataPrevMillis > 15000 || sendDataPrevMillis == 0)){
     sendDataPrevMillis = millis();
     // Write an Int number on the database path test/int
     if (Firebase.RTDB.setInt(&fbdo, "test/int", count)){
