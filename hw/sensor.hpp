632f18e0f1f9efae2af47baf523cb958016a9a84
lunakaray <93786059+lunakaray@users.noreply.github.com>
2023-01-15 16:17:28 +0200

    added switch functionality

--- hw/sensor.hpp
+++ hw/sensor.hpp
@@ -1,33 +1,35 @@
 // Endstop
 int endstop_1 = 25;
 int endstop_2 = 5;
-int ledPin = 25;
+unsigned long bS = 0;
 unsigned long pm =0 ;
-unsigned long interval = 0;
-unsigned long  ledState = LOW;
+unsigned long interval = 25;
+unsigned long  ledState = HIGH;
 void setup() {
     pinMode(endstop_1, INPUT);
     pinMode(endstop_2, OUTPUT);
+    digitalWrite(endstop_2,HIGH);
     Serial.begin(9600);
 }
 
-void loop() {
+ void loop() {
   unsigned long cM = millis ();
   if (ledState == LOW){
-    digitalWrite(ledPin , HIGH);
-    //bS=digitalRead(18);
-    digitalWrite(ledPin, LOW);
-    delay(5);
+    digitalWrite(endstop_2 , HIGH);
+    bS=digitalRead(25);
+    digitalWrite(endstop_2, LOW);
+    delay(15);
   }else {
-    //bS=digitalRead(18);
+    bS=digitalRead(25);
   }
   if(cM-pm>=interval){
     pm=cM;
+  //  delay(15);
     if(ledState ==LOW){
       ledState = HIGH;
     }else {
       ledState = LOW;
     }
-    digitalWrite(ledPin,ledState);
+    digitalWrite(endstop_2,ledState);
   }
 }
