3fd6d3ed837ae3f8a2a6feaea0b4af78b39ade7e
lunakaray <93786059+lunakaray@users.noreply.github.com>
2023-02-15 18:08:07 +0200

    temp

--- hw/sensor.hpp
+++ hw/sensor.hpp
@@ -1,7 +1,9 @@
 // Endstop
 #include <Arduino.h>
+#include <Adafruit_PCF8574.h>
 class sensor {
   public :
+  Adafruit_PCF8574 pcf;
   int endstop_1 = 25;// input green wire
   int endstop_2 = 5; // output red wire
   unsigned long  ledState = HIGH;
@@ -12,13 +14,13 @@ class sensor {
 
 
 unsigned long  ledState = HIGH;
-void sensor::init(int endstop_1, int endstop_2) {
+void sensor::init(int endstop_1, int endstop_2,Adafruit_PCF8574& pcf) {
     this->endstop_1=endstop_1;
     this->endstop_2=endstop_2;
     this->ledState=HIGH;
-    pinMode(endstop_1, INPUT);
-    pinMode(endstop_2, OUTPUT);
-    digitalWrite(endstop_2,HIGH);
+    pcf.pinMode(endstop_1, INPUT);
+    pcf.pinMode(endstop_2, OUTPUT);
+    pcf.digitalWrite(endstop_2,HIGH);
     Serial.begin(9600);
 }
 
@@ -26,12 +28,13 @@ void sensor::init(int endstop_1, int endstop_2) {
   unsigned long bS = 0;
 
   if (ledState == LOW){
-    digitalWrite(endstop_2 , HIGH);
-    delay(1);
-    bS=digitalRead(this->endstop_1);    
+    pcf.digitalWrite(endstop_2 , HIGH);
+    delay(100);
+    bS=pcf.digitalRead(this->endstop_1);    
     digitalWrite(endstop_2, LOW);
-    delay(500);
+    delay(100);
   }else {
+    delay(100);
     bS=digitalRead(this->endstop_1);
   }
 
